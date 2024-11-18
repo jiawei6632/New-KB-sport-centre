9e7049511c21fa9a16d693bb5836d556f1d99664
towjiawei <tow_jia_wei@dk=schweizer.com>
2024-11-18 16:41:51 +0800

    add (theme dark and light mode, but cannot run)

--- src/app/functions/header/header.component.ts
+++ src/app/functions/header/header.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
+// import { ThemeComponent } from '../theme/theme.component';
 import { PrimeIcons, MenuItem } from 'primeng/api';
 // import { LoginBtnComponent } from '../login-btn/login-btn.component';
 // import { ProfileComponent } from '../profile/profile.component';
@@ -11,6 +12,7 @@ import { PrimeIcons, MenuItem } from 'primeng/api';
   imports: [
     MenubarModule,
     RouterModule,
+    // ThemeComponent,
     // SidebarComponent,
     // LoginBtnComponent,
     // ProfileComponent,
